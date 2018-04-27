ca295decb2897b28a540f9f500b0f8315bc8f53f
Jeammy <37309589+Jeammy@users.noreply.github.com>
2018-04-27 09:06:09 -0400

    Update main.cpp

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -1,9 +1,9 @@
-include <IOStream>
+#include <ioStream.h>
 void main (){
 	//commentaire
 	cout >> "hello world";
 	cout >> "bonjour le monde";
 	cout >> "modification fait par quelqu'un d'autres";
 	cout >> "ajout de branch!";
-
+	cout >> "autre modification
 }
